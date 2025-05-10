9d3d87853ee1f18c2f0158d26678e85e7e327dc5
Kabir Sidhu <kxbir414@gmail.com>
2025-05-10 16:54:27 -0700

    init commit

---
+++ src/main/java/com/anisha/anisha_website/AnishaWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.anisha.anisha_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnishaWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnishaWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anisha/anisha_website/AnishaWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anisha.anisha_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnishaWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
